4f21e8039b1881fbd0001eeacfde91c1cc3915a0
FernandoS2500 <fernando.salazar.2500@mail.linnbenton.edu>
2024-04-06 16:39:45 -0700

    name change

---
+++ Assignment1/bag-of-marbles/marbles.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+// comment how
+
+int main() {
+    string marbleinfo;
+    cout << "Enter the marble color: " ;
+    cin >> marbleinfo;
+    cout << "Your marble color is: " << marbleinfo;
+    
+    return 0;
+}
