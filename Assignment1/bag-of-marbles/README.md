e94343dbfe92432429784a4622df25e3bb3cfe3e
FernandoS2500 <fernando.salazar.2500@mail.linnbenton.edu>
2024-04-06 20:46:04 -0700

    marbles major update
    fleshed out marble and marbles bag class, with functions added

--- Assignment1/bag-of-marbles/README.md
+++ Assignment1/bag-of-marbles/README.md
@@ -2,7 +2,7 @@
 Fernando Salazar
 
 
-Marbles are going to be represnted by var that has the color, size and some id that is assigned by the program
+Marbles are going to be represnted by var that has the color, size and maybe some id that is assigned by the program
 
 A function that will add new marbles, each marble added must have the color and size.
 
@@ -10,7 +10,7 @@ A function to remove a marble, Will be done by assigned ID, not sure if it will 
 Need to check if bag is empty before attemping remove marble
 
 
-A function to list all the marbles with ID and amount in the bag.
+A function to list all the marbles with ID 
 
 Im not sure yet IF the marbles information will be stored in a list or someother method yet to test the functions for adding and removal. I will check marble count. and if all the fields are filled.
 
@@ -19,4 +19,20 @@ For the list function , I can compare what the list should be to what has been a
 All of this will be done using the cmd prompt for input.
 
 // How to use
+
+Run the program in the cmd prompt from its directory by typing .\marbles.exe
+
+When the program loads, the default menu gives you 4 options.
+
+1. Add a marble
+2. Remove a marble
+3. Show all marbles
+4. Exit
+
+Enter 1-4 based of what you want to do
+
+After each function is finished running it will go back to main menu till the program 
+is exited.
+
+
 
--- Assignment1/bag-of-marbles/marbles.cpp
+++ Assignment1/bag-of-marbles/marbles.cpp
@@ -2,13 +2,102 @@
 #include <string>
 using namespace std;
 
-// comment how
+//  object class/contructor for marble
+class Marble {
+public:
+    string color;
+    string size;
 
-int main() {
-    string marbleinfo;
-    cout << "Enter the marble color: " ;
-    cin >> marbleinfo;
-    cout << "Your marble color is: " << marbleinfo;
+    Marble(string colo = "", string siz = "") {
+        //  assign the values
+        color = colo;
+        size = siz;
+    }
+};
+
+class BagOfMarbles {
+public:
+    // array for bag of marbles, hard code cap
+    Marble marbles[10];
+    int count;
     
+    //function for adding marbles
+    void addMarble() {
+        //checks if more marbles can be added
+        if (count >= 10) {
+            cout << "Bag is full." << endl;
+            return;
+        }
+        // ask for user input
+        string color, size;
+        cout << "Enter marble color: ";
+        cin >> color;
+        cout << "Enter marble size: ";
+        cin >> size;
+        // save  input to array, auto adds count up
+        marbles[count++] = Marble(color, size);
+        cout << "Marble added: Color = " << color << ", Size = " << size << endl;
+    }
+    // remove marvel function
+    void removeMarble() {
+        // checks if bag is empty 
+        if (count <= 0) {
+            cout << "Bag is empty." << endl;
+            return;
+        }
+        // remove the last item  put in bag, kinda still there till it get over writen
+        count--;
+         // maybe add choice option later
+         //print for whats being removed
+        cout << "Marble removed: Color = " << marbles[count].color << ", Size = " << marbles[count].size << endl;
+    }
+    // list all marble functions
+    void showMarbles() {
+         // checks if bag is empty 
+        if (count == 0) {
+            cout << "The bag is empty." << endl;
+            return;
+        }
+        // loops through the marbles in bag
+        for (int i = 0; i < count; i++) {
+            cout << "Marble " << i+1 << ": Color = " << marbles[i].color << ", Size = " << marbles[i].size << endl;
+        }
+    }
+};
+// main loop
+int main() {
+    // create object
+    BagOfMarbles bag;
+    char choice;
+
+    // menu for all choices.
+    while (true) {
+        cout << "\nChoose an action:\n"
+             << "1. Add a marble\n"
+             << "2. Remove a marble\n"
+             << "3. Show all marbles\n"
+             << "4. Exit\n"
+             << "Enter your choice (1-4): ";
+        cin >> choice;
+        // active function based of user choice
+        switch (choice) {
+            case '1':
+                bag.addMarble();
+                break;
+            case '2':
+                bag.removeMarble();
+                break;
+            case '3':
+                bag.showMarbles();
+                break;
+            case '4':
+                cout << "Good Bye." << endl;
+                return 0;
+            default:
+                cout << "Please enter 1, 2, 3, or 4." << endl;
+                break;
+        }
+    }
+
     return 0;
 }
